6c9f1bbcb0bfba51b6490dbd218d3b730e970753
Matheus Tenorio dos Santos <tenoriomatheus0@gmail.com>
2018-07-04 15:19:43 -0300

    Changing queue to list in SharedIDPool class and add a unit test to SharedIDPool class
    Signed-off-by: Matheus Tenorio dos Santos <tenoriomatheus0@gmail.com>

--- core/shared_id_pool.py
+++ core/shared_id_pool.py
@@ -1,30 +1,28 @@
 #!/usr/bin/python3
-from queue import Queue
 from threading import Lock
 import random
 
 
-# TODO: Change queue for other data structure
+# TODO: Change list for other data structure
 # TODO: Add a unit test to this class
 class SharedIDPool:
 
     def __init__(self, max_size):
         self.max_size = max_size
-        self.dirty_ids = Queue(max_size)
+        self.dirty_ids = []
+        self.dirty_ids_size = max_size
         self.lock = Lock()
 
     def get_new_id(self):
         self.lock.acquire()
 
-        if self.dirty_ids.full():
-            self.dirty_ids = Queue()
-
-        dirty_ids = iter(self.dirty_ids.get_nowait())
+        if len(self.dirty_ids) == self.dirty_ids_size:
+            self.dirty_ids.clear()
 
         id_ = random.randrange(0, self.max_size)
-        while id_ in dirty_ids:
+        while id_ in self.dirty_ids:
             id_ = random.randrange(0, self.max_size)
 
-        self.dirty_ids.put_nowait(id_)
+        self.dirty_ids.append()
         self.lock.release()
         return id_
