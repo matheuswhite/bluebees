f47e8377f10303e9fe6e2ea69c91d1f1dee487da
Matheus Tenorio dos Santos <tenoriomatheus0@gmail.com>
2018-07-05 11:24:46 -0300

    Doing read/notify method
    Signed-off-by: Matheus Tenorio dos Santos <tenoriomatheus0@gmail.com>

--- core/pb_adv.py
+++ core/pb_adv.py
@@ -3,6 +3,7 @@ import math
 
 from core.shared_id_pool import SharedIDPool
 from core.provisioning_link import *
+from core.event_system import Event, Subscriber
 
 
 class InvalidParameterType(Exception):
@@ -29,14 +30,17 @@ class PBADVPDU:
 
 # TODO: Create a method to handle an incoming close message
 # TODO: Create a method to wait the ack response. Send close case not ack has been arise
-class PBADV:
+class PBADV(Subscriber):
 
-    def __init__(self, write_cb, read_cb):
+    def __init__(self, write_cb, new_pbadvpdu_event):
         self.write_cb = write_cb
-        self.read_cb = read_cb
+        self.new_pbadvpdu_event = new_pbadvpdu_event
+        self.new_gppdu_event = Event()
         self.link_ids = SharedIDPool(0xFFFFFFFF)
         self.links = []
         self.MTU_SIZE = 24
+
+        self.subscribe(self.new_pbadvpdu_event)
 
     def open(self, device_uuid: bytes):
         # Creating a new link
@@ -74,9 +78,43 @@ class PBADV:
             pdu = PBADVPDU(link.get_link_id(), link.get_a_transaction_number(), segment)
             self.write_cb(pdu.to_bytes())
 
+    # TODO: Test it
+    def notify(self, data: bytes):
+        pdu = PBADVPDU(link_id=data[:4], transaction_number=data[5:6], payload=data[6:])
+
+        if pdu.payload[0] == LINK_CLOSE:
+            self.handle_incoming_link_close(pdu)
+        else:
+            self.new_gppdu_event.notify(pdu.payload)
+
+    def get_new_gppdu_event(self):
+        return self.new_gppdu_event
+
     # TODO: Implement
-    def read(self):
+    def handle_incoming_link_close(self, pdu):
+        if pdu.payload[1] == LINK_CLOSE_SUCESS:
+            self.remove_link_by_link_id(pdu.link_id)
+        elif pdu.payload[1] == LINK_CLOSE_TIMEOUT:
+            pass
+        elif pdu.payload[1] == LINK_CLOSE_FAIL:
+            pass
+        else:
+            pass
+
         raise NotImplemented
+
+    # TODO: Test it
+    def remove_link_by_link_id(self, link_id):
+        p_link = None
+        for link in self.links:
+            if link.get_link_id() == link_id:
+                p_link = link
+                break
+
+        if p_link is None:
+            raise LinkNotFoundError(ProvisioningLink(link_id, None, True))
+        else:
+            self.links.remove(p_link)
 
     def segment_payload(self, payload):
         payload_length = len(payload)
