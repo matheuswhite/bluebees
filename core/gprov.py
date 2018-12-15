ae079a7575f5ae9d307b15b0333600f933485dfc
Matheus "White" Tenório dos Santos <tenoriomatheus0@gmail.com>
2018-12-15 18:03:32 -0300

    Scheduling done and doing gprov
    Signed-off-by: Matheus "White" Tenório dos Santos <tenoriomatheus0@gmail.com>

--- core/gprov.py
+++ core/gprov.py
@@ -4,6 +4,9 @@ from core.transaction import Transaction
 from threading import Event
 from time import sleep
 from core.log import Log
+from core.scheduling import scheduler, TaskTimer
+from core.utils import crc8
+from core.dongle import MAX_MTU
 
 
 log = Log('Gprov')
@@ -177,3 +180,175 @@ class Gprov:
         log.log(b'Send: ' + self.link.get_adv_header() + b'\x03' + self.link.device_uuid)
         self.driver.send(0, 20, self.link.get_adv_header() + b'\x03' + self.link.device_uuid)
         sleep(self.link_open_retransmit_delay)
+
+
+class DeviceConnection:
+
+    def __init__(self, link_id):
+        self.link_id = link_id
+        self.prov_tr_number = 0x00
+        self.device_tr_number = 0x80
+        self.recv_transactions = []
+        self.messages = {}
+        self.cache = []
+        self.tr_status = 'start'
+        self.fcs = 0
+        self.tr_len = 0
+        self.segn = 0
+        self.clear_cache_timeout = 30
+        self.is_alive = True
+
+        scheduler.spawn_task(f'clear_cache_task{link_id}', self._clear_cache_task)
+
+    def _clear_cache_task(self):
+        while self.is_alive:
+            clear_timer = TaskTimer()
+            clear_timer.timeout = self.clear_cache_timeout
+            scheduler.wait_timer(f'_clear_cache_task{self.link_id}', clear_timer)
+            yield
+            self.cache = []
+
+    def _get_last_transaction_task(self):
+        while len(self.recv_transactions) == 0:
+            yield
+        last_recv_transaction = self.recv_transactions[0]
+        self.recv_transactions = self.recv_transactions[1:]
+        yield last_recv_transaction
+
+    def _is4me(self, content: bytes):
+        return content[0:4] == int(self.link_id).to_bytes(4, 'big')
+
+    def _already_in_cache(self, content: bytes):
+        return content in self.cache
+
+    def _is_tr_ack(self, content: bytes):
+        return content[4] == self.prov_tr_number and content[5] == 0x01
+
+    def _has_correct_tr_number(self, content: bytes):
+        return content[4] == self.device_tr_number
+
+    def _validate_message(self):
+        # remount transaction
+        tr_content = b''
+        x = 0
+        while self.messages:
+            tr_content += self.messages[x]
+            del self.messages[x]
+            x += 1
+
+        # check total length
+        if self.tr_len != len(tr_content):
+            return
+
+        # check fcs
+        calc_fcs = crc8(tr_content)
+        if self.fcs != calc_fcs:
+            return
+
+        # add transaction
+        self.recv_transactions.append(tr_content)
+
+        self.tr_status = 'start'
+
+    def add_recv_message(self, content: bytes):
+        if not self._is4me(content):
+            return
+        if self._already_in_cache(content):
+            return
+        if self._is_tr_ack(content):
+            self.prov_tr_number += 1
+            return
+        if not self._has_correct_tr_number(content):
+            return
+
+        content = content[5:]
+
+        if self.tr_status == 'start':
+            first_byte = content[0]
+            if first_byte & 0x03 != 0:
+                return
+
+            self.segn = (first_byte & 0xfc) >> 2
+            self.tr_len = int.from_bytes(content[1:3], 'big')
+            self.fcs = content[3]
+            self.messages[0] = content[4:]
+        elif self.tr_status == 'continuation':
+            first_byte = content[0]
+            if first_byte & 0x03 != 2:
+                self.tr_status = 'start'
+                return
+
+            seg_index = (first_byte & 0xfc) >> 2
+            self.messages[seg_index] = content[1:]
+
+        if len(self.messages) - 1 < self.segn:
+            self.tr_status = 'continuation'
+        else:
+            self._validate_message()
+
+        self.cache.append(content)
+
+    # def _usage_task(self):
+    #     msgs = device_conn.mount_snd_transaction(b'')
+    #     for m in msgs:
+    #         driver.send(m)
+    #
+    #     ret = []
+    #     device_conn.get_last_transaction('_usage', ret)
+    #     yield
+    #
+    #     content = ret[0]
+
+    def get_last_transaction_task(self, invoker_name: str, ret_queue: list):
+        spawned_task_name = f'_get_last_transaction_task{self.link_id}'
+        scheduler.spawn_task(spawned_task_name, self._get_last_transaction_task(), ret_queue)
+        scheduler.set_dependency(invoker_name, spawned_task_name)
+
+    def mount_snd_transaction(self, content: bytes):
+        messages = []
+
+        if len(content) == 0:
+            return messages
+
+        header = int(self.link_id).to_bytes(4, 'big') + int(self.prov_tr_number).to_bytes(1, 'big')
+
+        # start message
+        total_seg_number = int((len(content) - 1)/MAX_MTU)
+        segn = (total_seg_number << 2).to_bytes(1, 'big')
+        total_length = len(content).to_bytes(2, 'big')
+        fcs = crc8(content).to_bytes(1, 'big')
+        has_continuation = total_length > MAX_MTU
+        if has_continuation:
+            data = content[0:MAX_MTU]
+            content = content[MAX_MTU:]
+        else:
+            data = content
+        messages.append(header + segn + total_length + fcs + data)
+
+        # continuation messages
+        if has_continuation:
+            for i in range(1, total_seg_number):
+                seg_index = ((i << 2) | 0x02).to_bytes(1, 'big')
+                data = content[0:MAX_MTU]
+                content = content[MAX_MTU:]
+                messages.append(header + seg_index + data)
+            seg_index = ((total_seg_number << 2) | 0x02).to_bytes(1, 'big')
+            messages.append(header + seg_index + content)
+
+        return messages
+
+    # TODO
+    def open_connection(self):
+        raise NotImplementedError
+
+    # TODO
+    def close_connection(self):
+        raise NotImplementedError
+
+
+class GenericProvisioner:
+
+    def __init__(self, dongle_driver):
+        self.driver = dongle_driver
+        self.connections = []
+
--- core/scheduling.py
+++ core/scheduling.py
@@ -1,60 +1,104 @@
 from collections import deque
 from time import time
-from dataclasses import dataclass
-from core.log import Log, LogLevel
-from typing import List
 from threading import Event
 
 
-log = Log('Scheduler')
-log.level = LogLevel.Inf.value
+class TaskEvent:
+
+    def __init__(self):
+        self.event = Event()
 
 
-def timer_task(timeout_, event: Event):
-    elapsed_time = 0
-    while elapsed_time < timeout_:
-        ts = time()
-        yield
-        elapsed_time += time() - ts
-    event.set()
+class TaskTimer:
+
+    def __init__(self):
+        self.timeout = 0
+        self.ts = 0
+        self.te = 0
+
+
+class Task:
+
+    def __init__(self, name, ret_queue):
+        self.name = name
+        self.ret_queue = ret_queue
+        self.dependency = None
+        self.status = 'idle'
+        self.event = None
+        self.timer = None
+
+    def set_dependency(self, dependency):
+        self.dependency = dependency
+
+    def set_task_event(self, event: TaskEvent):
+        self.event = event
+
+    def set_task_timer(self, timer: TaskTimer):
+        self.timer = timer
 
 
 class Scheduler:
 
     def __init__(self):
-        self.tasks = deque()
-        self.tasks_dependency = {}
-        self.tasks_status = {}
+        self.jobs = deque()
+        self.tasks = {}
 
-    def new_task(self, name: str):
-        def wrap(func):
-            self.tasks.append((name, func()))
-            self.tasks_status[name] = 'running'
-            return func
-        return wrap
+    def _append_job(self, name, func):
+        self.jobs.append((name, func))
 
-    def add_dependency(self, name: str, dependency: str):
-        self.tasks_dependency[name] = dependency
+    def spawn_task(self, name: str, func, ret_queue=list()):
+        self._append_job(name, func)
+        self.tasks[name] = Task(name, ret_queue)
+
+    def set_dependency(self, name: str, dependency: str):
+        self.tasks[name].set_dependency(self.tasks[dependency])
+        self.tasks[name].status = 'wait_task'
+
+    def wait_event(self, name: str, event: TaskEvent):
+        event.ts = time()
+        self.tasks[name].set_task_event(event)
+        self.tasks[name].status = 'wait_event'
+
+    def wait_timer(self, name: str, timer: TaskTimer):
+        timer.ts = time()
+        self.tasks[name].set_task_timer(timer)
+        self.tasks[name].status = 'wait_timer'
 
     def run(self):
-        while self.tasks:
-            # pop next task
-            task_name, task = self.tasks.popleft()
+        while self.jobs:
+            # pop next job
+            job_name, job = self.jobs.popleft()
+            task = self.tasks[job_name]
 
-            # check dependencies
-            if task_name in self.tasks_dependency:
-                dependency = self.tasks_dependency[task_name]
-                if self.tasks_status[dependency] == 'running':
-                    self.tasks.append((task_name, task))
-                    continue
+            # check if job is waiting for other jobs
+            if task.status == 'wait_task' and task.dependency.status == 'finished':
+                task.status = 'idle'
 
-            try:
-                log.dbg(f'Start task')
-                next(task)
-                log.dbg(f'Yield task')
-                self.tasks.append((task_name, task))
-            except StopIteration:
-                self.tasks_status[task_name] = 'finished'
+            # check if job is waiting a event
+            if task.status == 'wait_event' and task.event.event.isSet():
+                task.status = 'idle'
+
+            # check if a job is waiting a timer
+            if task.status == 'wait_timer':
+                now_time = time()
+                task.timer.te += now_time - task.timer.ts
+                task.timer.ts = now_time
+                if task.timer.te > task.timer.timeout:
+                    task.status = 'idle'
+
+            if task.status == 'idle':
+                try:
+                    task.status = 'running'
+                    ret = next(job)
+                    if ret:
+                        task.ret_queue.append(ret)
+                    if task.status == 'running':
+                        task.status = 'idle'
+                    self._append_job(job_name, job)
+                except StopIteration:
+                    task.status = 'finished'
+            else:
+                self._append_job(job_name, job)
 
 
 scheduler = Scheduler()
@@ -62,21 +106,65 @@ scheduler = Scheduler()
 
 if __name__ == '__main__':
 
-    @scheduler.new_task('task_in')
-    def task_in():
-        n = 10
-        while n > 0:
-            print(f'Runing in task {n}')
-            yield
-            n -= 1
+    class Tester:
 
-    @scheduler.new_task('task_out')
-    def task_out():
-        n = 5
-        while n > 0:
-            print(f'Task out executing {n}')
-            scheduler.add_dependency('task_out', 'task_in')
-            yield
-            n -= 1
+        def __init__(self):
+            self.evt = TaskEvent()
+            self.timer_ = TaskTimer()
+            self.timer_.timeout = 3
+            self.ret_queue = []
 
+            scheduler.spawn_task('task_wait_event', self.task_wait_event())
+            scheduler.spawn_task('task_dependency', self.task_dependency())
+            scheduler.spawn_task('task_wait_timer', self.task_wait_timer())
+            scheduler.spawn_task('task_with_return', self.task_with_return(), ret_queue=self.ret_queue)
+
+        def task_set_event(self):
+            n = 10
+            while n > 0:
+                print(f'Running set event task {n}')
+                yield
+                n -= 1
+            self.evt.event.set()
+
+        def task_wait_event(self):
+            n = 5
+            scheduler.wait_event('task_wait_event', self.evt)
+            yield
+            while n > 0:
+                print(f'Running wait event {n}')
+                yield
+                n -= 1
+
+        def task_dependency(self):
+            n = 5
+            scheduler.set_dependency('task_dependency', 'task_wait_event')
+            yield
+            while n > 0:
+                print(f'Running dependency {n}')
+                yield
+                n -= 1
+
+        def task_wait_timer(self):
+            n = 5
+            scheduler.wait_timer('task_wait_timer', self.timer_)
+            yield
+            while n > 0:
+                print(f'Running wait timer {n}')
+                yield
+                n -= 1
+
+        def task_with_return(self):
+            n = 5
+            while n > 0:
+                print(f'Running with return {n}')
+                yield
+                n -= 1
+            yield 200
+
+
+    tester = Tester()
+
+    scheduler.spawn_task('task_set_event', tester.task_set_event())
     scheduler.run()
+    print(tester.ret_queue)
