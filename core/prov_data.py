8466ea5e30649f9083dd4f301118eef52bf5a862
Matheus "White" Tenório dos Santos <tenoriomatheus0@gmail.com>
2019-01-11 10:28:08 -0300

    Provisioning Data file manager tested #adzvg[closed]
    Signed-off-by: Matheus "White" Tenório dos Santos <tenoriomatheus0@gmail.com>

--- core/prov_data.py
+++ core/prov_data.py
@@ -23,25 +23,33 @@ class ProvData:
         self.iv_index = iv_index
         self.unicast_address = unicast_address
 
+    @classmethod
+    def bytes2str(cls, value: bytes):
+        return value.decode('utf8')
+
+    @classmethod
+    def str2bytes(cls, value: str):
+        return value.encode('utf8')
+
     def save(self, filename: str):
         with open(filename, 'w') as file:
             data = {
-                'prov_public_key_x': self.prov_public_key_x,
-                'prov_public_key_y': self.prov_public_key_y,
-                'dev_public_key_x': self.dev_public_key_x,
-                'dev_public_key_y': self.dev_public_key_y,
-                'prov_priv_key': self.prov_priv_key,
-                'random_provisioner': self.random_provisioner,
-                'random_device': self.random_device,
-                'auth_value': self.auth_value,
-                'invite_pdu': self.invite_pdu,
-                'capabilities_pdu': self.capabilities_pdu,
-                'start_pdu': self.start_pdu,
-                'network_key': self.network_key,
-                'key_index': self.key_index,
-                'flags': self.flags,
-                'iv_index': self.iv_index,
-                'unicast_address': self.unicast_address
+                'prov_public_key_x': self.bytes2str(self.prov_public_key_x),
+                'prov_public_key_y': self.bytes2str(self.prov_public_key_y),
+                'dev_public_key_x': self.bytes2str(self.dev_public_key_x),
+                'dev_public_key_y': self.bytes2str(self.dev_public_key_y),
+                'prov_priv_key': self.bytes2str(self.prov_priv_key),
+                'random_provisioner': self.bytes2str(self.random_provisioner),
+                'random_device': self.bytes2str(self.random_device),
+                'auth_value': self.bytes2str(self.auth_value),
+                'invite_pdu': self.bytes2str(self.invite_pdu),
+                'capabilities_pdu': self.bytes2str(self.capabilities_pdu),
+                'start_pdu': self.bytes2str(self.start_pdu),
+                'network_key': self.bytes2str(self.network_key),
+                'key_index': self.bytes2str(self.key_index),
+                'flags': self.bytes2str(self.flags),
+                'iv_index': self.bytes2str(self.iv_index),
+                'unicast_address': self.bytes2str(self.unicast_address)
             }
             json.dump(data, file)
 
@@ -49,7 +57,7 @@ class ProvData:
     def load(cls, filename: str):
         with open(filename, 'r') as file:
             data = json.load(file)
-            return cls(data['prov_public_key_x'], data['prov_public_key_y'], data['dev_public_key_x'],
-                        data['dev_public_key_y'], data['prov_priv_key'], data['random_provisioner'], data['random_device'],
-                        data['auth_value'], data['invite_pdu'], data['capabilities_pdu'], data['start_pdu'],
-                        data['network_key'], data['key_index'],  data['flags'], data['iv_index'], data['unicast_address'])
+            return cls(cls.str2bytes(data['prov_public_key_x']), cls.str2bytes(data['prov_public_key_y']), cls.str2bytes(data['dev_public_key_x']),
+                        cls.str2bytes(data['dev_public_key_y']), cls.str2bytes(data['prov_priv_key']), cls.str2bytes(data['random_provisioner']), cls.str2bytes(data['random_device']),
+                        cls.str2bytes(data['auth_value']), cls.str2bytes(data['invite_pdu']), cls.str2bytes(data['capabilities_pdu']), cls.str2bytes(data['start_pdu']),
+                        cls.str2bytes(data['network_key']), cls.str2bytes(data['key_index']),  cls.str2bytes(data['flags']), cls.str2bytes(data['iv_index']), cls.str2bytes(data['unicast_address']))
