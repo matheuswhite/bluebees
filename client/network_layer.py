6ee92edd77570c803738175e754d349a4b81cec7
Matheus "White" Tenório dos Santos <tenoriomatheus0@gmail.com>
2019-01-26 21:39:49 -0300

    Implementing network layer crypto
    Signed-off-by: Matheus "White" Tenório dos Santos <tenoriomatheus0@gmail.com>

--- client/network_layer.py
+++ client/network_layer.py
@@ -22,6 +22,7 @@ class NetworkLayer:
         self.iv_index = 0x0000_0000
         self.seq = 0x00_0000
         self.network_keys = {}
+        self.network_key_lookup_table = {}
 
     def _gen_security_material(self, network_id: bytes):
         materials = CRYPTO.k2(self.network_keys[network_id], b'\x00')
@@ -30,41 +31,46 @@ class NetworkLayer:
         privacy_key = materials & PRIVACY_KEY_MASK
         return nid, encryption_key, privacy_key
 
-    def _gen_netmic(self, is_control_message: bool, dst: bytes, transport_pdu: bytes):
-        # net_mic = b'\x00\x00\x00\x00' or b'\x00\x00\x00\x00\x00\x00\x00\x00'
-        raise NotImplementedError
-
-    def _encrypt(self, dst: bytes, transport_pdu: bytes):
-        raise NotImplementedError
-
-    def _encrypt2(self, enc_dst: bytes, enc_transport_pdu: bytes):
+    def _encrypt(self, is_control_message: bool, dst: bytes, transport_pdu: bytes):
         raise NotImplementedError
 
     def _obsfucate(self, ctl, ttl, seq, src):
         raise NotImplementedError
 
-    def _authenticate(self):
+    def _clean_message(self, obfuscated):
+        raise NotImplementedError
+
+    def _authenticate(self, encrypted_data: bytes, net_mic: bytes, encryption_key: bytes, network_nonce: bytes):
         raise NotImplementedError
 
     def _handle_recv_message(self, msg):
         ivi = (msg[0] & 0x80) >> 7
         nid = msg[0] & 0x7f
-        encryption_key = 0 # ???
-        privacy_key = 0 # ???
-        ctl = (msg[1] & 0x80) >> 7
-        ttl = msg[1] & 0x7f
-        seq = msg[2:5]
-        src = msg[5:7]
-        dst = msg[7:9]
-        transport_pdu = msg[9:-4] if ctl == 0 else msg[9:-8]
-        net_mic = msg[-4:] if ctl == 0 else msg[-8:]
+        encryption_key = self.network_key_lookup_table[nid][0]
+        privacy_key = self.network_key_lookup_table[nid][1]
+        obfuscated = msg[1:7]
 
-        if not self._authenticate():
+        clean = self._clean_message(obfuscated)
+        ctl = (clean[0] & 0x80) >> 7
+        ttl = clean[0] & 0x7f
+        seq = clean[1:4]
+        src = clean[4:6]
+        if seq <= self.seq:
+            return
+        else:
+            self.seq = seq
+
+        net_mic = msg[-4:] if ctl == 0 else msg[-8:]
+        encrypted_data = msg[9:-4] if ctl == 0 else msg[9:-8]
+        network_nonce = b'\x00' + clean + b'\x00\x00' + self.iv_index.to_bytes(4, 'big')
+        is_valid, dst, transport_pdu = self._authenticate(encrypted_data, net_mic, encryption_key, network_nonce)
+
+        if is_valid:
             return
 
-        msg = transport_pdu
+        # TODO: Check if src and dst address is valid
 
-        self.new_message_signal.emit(msg=msg)
+        self.new_message_signal.emit(msg=(src, dst, transport_pdu))
 
     def kill(self):
         self.is_alive = False
@@ -82,6 +88,8 @@ class NetworkLayer:
         if not (1 <= len(transport_pdu) <= 16):
             return
 
+        # TODO: Check if src and dst address is valid
+
         ivi = (self.iv_index & 0x01) << 7
         nid, encryption_key, privacy_key = self._gen_security_material(network_id)
         ctl = 0x80 if is_control_message else 0x00
@@ -89,20 +97,20 @@ class NetworkLayer:
         seq = self.seq
         src = src_addr.byte_value
         dst = dst_addr.byte_value
-        net_mic = self._gen_netmic(is_control_message, dst, transport_pdu)
 
-        enc_dst, enc_transport_pdu = self._encrypt(dst, transport_pdu)
-        encrypted = self._encrypt2(enc_dst, enc_transport_pdu)
+        self.network_key_lookup_table[nid] = (encryption_key, privacy_key)
+        enc_dst, enc_transport_pdu, net_mic = self._encrypt(is_control_message, dst, transport_pdu)
         obsfucated = self._obsfucate(ctl, ttl, seq, src)
 
-        network_pdu = (ivi | nid).to_bytes(1, 'big') + obsfucated + encrypted + net_mic
+        network_pdu = (ivi | nid).to_bytes(1, 'big') + obsfucated + enc_dst + enc_transport_pdu + net_mic
 
         self.driver.send(2, 20, network_pdu)
+
+        self.seq += 1
 
     def recv_transport_pdu_t(self, self_task: Task):
         while self.is_alive:
             msg = self.driver.recv('message')
-            yield
             if msg is not None:
                 self._handle_recv_message(msg)
             yield
