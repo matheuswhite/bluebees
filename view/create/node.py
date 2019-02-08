238a74e84f2aaced213e605362ac871bdf1bd2dd
Matheus "White" Tenório dos Santos <tenoriomatheus0@gmail.com>
2019-02-07 22:06:53 -0300

    Add conditional option view's element
    Signed-off-by: Matheus "White" Tenório dos Santos <tenoriomatheus0@gmail.com>

--- view/create/node.py
+++ view/create/node.py
@@ -1,6 +1,7 @@
 from view.page import Page
 from view.question import *
 from view.options import *
+from model.mesh_manager import mesh_manager
 
 
 def _tf_check(answer: str):
@@ -16,7 +17,22 @@ def _get_devices():
     return ['device 1234', 'device 1024', 'device 5991']
 
 
+def _list_networks():
+    return list(mesh_manager.networks.keys())
+
+
 create_node_page = Page(arguments=['create', 'node'])
 create_node_page += Question(question='Qual o nome do nó que será criado', end_quote=Q_MARK,
                              valid_answer_check=_node_name_check)
-create_node_page += Options(description='Escolha o dispositivo que deseja provisionar', dynamic_options=_get_devices)
+create_node_page += ConditionalOptions(description='Escolha qual das redes abaixo o nó fará parte:',
+                                       fail_description="Nenhuma rede encontrada. Por favor, crie uma rede antes de "
+                                                        "criar um nó, com o comando 'create network'",
+                                       dynamic_options=_list_networks)
+create_node_page += ConditionalOptions(description='Escolha o dispositivo alvo', fail_description="Nenhum dispositivo "
+                                                                                                  "encontrado. "
+                                                                                                  "Por favor, "
+                                                                                                  "use o comando "
+                                                                                                  "'scan devices' "
+                                                                                                  "para encontrar "
+                                                                                                  "novos dipositivos",
+                                       dynamic_options=_get_devices)
