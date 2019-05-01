5be44d0311f8e507562bd0142e76be738b2c31a7
Matheus T. dos Santos <tenoriomatheus0@gmail.com>
2019-05-01 11:40:31 -0300

    Update dongle class
    Signed-off-by: Matheus T. dos Santos <tenoriomatheus0@gmail.com>

--- core/dongle.py
+++ core/dongle.py
@@ -1,137 +1,118 @@
 import asyncio
-import zmq.asyncio
 import base64 as b64
-from zmq.asyncio import Context
 from asyncserial import Serial
 from dataclasses import dataclass
+from common.client import Client
 
 
 @dataclass
 class DongleMessage:
     msg_type: bytes
     content: bytes
+
+
+@dataclass
+class SerialMessage:
+    msg_type: bytes
+    xmit: bytes
+    intms: bytes
+    content_b64: bytes
     address: bytes
 
 
-class Dongle:
+class Dongle(Client):
 
-    def __init__(self, loop, serial_port, baudrate=115200, port=9521):
+    def __init__(self, loop, serial_port, baudrate=115200):
+        super().__init__(sub_topic_list=[b'message_s', b'prov_s'],
+                         pub_topic_list=[b'message', b'prov', b'beacon'])
         self.loop = loop
         self.serial_port = serial_port
         self.baudrate = baudrate
         self.serial = Serial(self.loop, self.serial_port, self.baudrate)
 
-        self.port = port
-        self.url = 'tcp://127.0.0.1:{self.port}'
-        self.ctx = Context.instance()
+        self.caches = {
+            b'message': [],
+            b'beacon': [],
+            b'prov': []
+        }
 
-        self.pub = self.ctx.socket(zmq.PUB)
-        self.sub = self.ctx.socket(zmq.SUB)
+        self.all_tasks += [self._read_from_serial(), self._write_on_serial()]
 
-        self.publish_queue = asyncio.Queue()
-        self.subscribe_queue = asyncio.Queue()
+    async def _read_serial_task(self):
+        while True:
+            serial_msg = await self._read_from_serial()
 
-        self.beacon_cache = []
-        self.message_cache = []
-        self.prov_cache = []
+            dongle_msg = self._translate_serial_message(serial_msg)
 
-    def _decode_message_type(self, msg_type):
-        if msg_type == b'prv':
-            return b'prov'
-        elif msg_type == b'msg':
-            return b'message'
+            await self.messages_to_send((dongle_msg.msg_type, dongle_msg))
 
-    def _encode_message_type(self, msg_type):
-        if msg_type == b'beacon':
-            return b'bcn'
-        elif msg_type == b'prov':
-            return b'prv'
-        elif msg_type == b'message':
-            return b'msg'
+    async def _write_serial_task(self):
+        while True:
+            (_, dongle_msg) = await self.messages_received.get()
+
+            serial_msg = self._translate_dongle_message(dongle_msg)
+
+            await self._write_on_serial(serial_msg)
 
     async def _read_from_serial(self):
+        line = b''
         while True:
-            serial_content = await self.serial.readline()
+            data = await self.serial.read()
+            line += data
+            if b'\r\n' in line:
+                if line[0:1] != b'@':
+                    continue
 
-            if serial_content[0:1] != b'@':
-                continue
+                parts = line.split(b' ')
 
-            serial_content: bytes = serial_content[1:]
+                if len(parts) != 3:
+                    continue
 
-            parts = serial_content.split(b' ')
-            if len(parts) != 3:
-                continue
+                msg = SerialMessage(msg_type=parts[0][1:], xmit=None,
+                                    intms=None,
+                                    content_b64=parts[1],
+                                    address=parts[2][:-2])
 
-            msg_type = parts[0][1:]
-            content = b64.b64decode(parts[1])
-            address = parts[2][:-2]
+                if msg in self.caches[msg.msg_type]:
+                    continue
+                self.caches[msg.msg_type].append(msg)
 
-            if msg_type not in [b'beacon', b'prov', b'message']:
-                continue
+                return msg
 
-            return DongleMessage(self._encode_message_type(msg_type),
-                                 content, address)
+    def _split_data(self, data: bytes):
+        datas = []
+        part = b''
 
-    async def _write_from_serial(self, type_: bytes, content: bytes):
-        msg_type = self._decode_message_type(type_)
-        xmit = 2
-        int_ms = 20
-        content_b64 = b64.encodebytes(content).decode('utf-8')[:-1]
+        for byte in data:
+            if len(part) >= 5:
+                datas.append(part)
+                part = b''
+            part += byte.to_bytes(1, 'big')
 
-        msg = f'@{msg_type} {xmit} {int_ms} {content_b64}\r\n'.encode('utf-8')
+        if len(part) > 0:
+            datas.append(part)
 
-        # ? I need divide in parts
-        await self.serial.write(msg)
+        return datas
 
-    async def read_task(self):
-        await self.serial.read()
+    async def _write_on_serial(self, msg: SerialMessage):
+        data = b'@' + msg.msg_type + b' ' + msg.xmit + b' ' + msg.intms + b' '
+        + msg.content_b64 + b'\r\n'
 
-        while True:
-            dongle_msg: DongleMessage = await self._read_from_serial()
+        datas = self._split_data(data)
+        for d in datas:
+            await self.serial.write(d)
+            await asyncio.sleep(0.005)
 
-            if dongle_msg.msg_type == b'bcn' \
-               and dongle_msg not in self.beacon_cache:
-                self.beacon_cache.append(dongle_msg)
-            elif dongle_msg.msg_type == b'prv' \
-                    and dongle_msg not in self.prov_cache:
-                self.prov_cache.append(dongle_msg)
-            elif dongle_msg.msg_type == b'msg' \
-                    and dongle_msg not in self.message_cache:
-                self.message_cache.append(dongle_msg)
+        await asyncio.sleep(0.1)
 
-            await self.publish_queue.put(dongle_msg)
+    def _translate_serial_message(self, msg: SerialMessage) -> DongleMessage:
+        dongle_msg = DongleMessage(msg_type=msg.msg_type,
+                                   content=b64.b64decode(msg.content_b64))
+        return dongle_msg
 
-    async def write_task(self):
-        while True:
-            msg: DongleMessage = await self.subscribe_queue.get()
-
-            # ! removing '_s' from message type
-            await self._write_from_serial(msg.msg_type[0:3], msg.content)
-
-    async def publish_task(self):
-        self.pub.connect(self.url)
-
-        await asyncio.sleep(0.3)
-
-        while True:
-            msg = await self.publish_queue.get()
-
-            await self.pub.send_multipart([msg.msg_type, msg.content])
-
-    async def subscribe_task(self):
-        self.sub.bind(self.url)
-
-        # ? Can I subscribe in 2 channels
-        self.sub.setsockopt(zmq.SUBSCRIBE, b'prv_s')
-        self.sub.setsockopt(zmq.SUBSCRIBE, b'msg_s')
-
-        while True:
-            [topic, msg] = await self.sub.recv_multipart()
-
-            dongle_msg = DongleMessage(topic, msg)
-
-            await self.subscribe_queue.put(dongle_msg)
-
-    def tasks(self):
-        return asyncio.gather(self.read_task(), self.write_task(),
-                              self.publish_task(), self.subscribe_task())
+    def _translate_dongle_message(self, msg: DongleMessage) -> SerialMessage:
+        serial_msg = SerialMessage(msg_type=msg.msg_type, xmit=b'2',
+                                   intms=b'20',
+                                   content_b64=b64.b64encode(msg.content),
+                                   address=None)
+        return serial_msg
