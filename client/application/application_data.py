13b88229c110ae40c61e7f58b187c07993568b77
Matheus T. dos Santos <tenoriomatheus0@gmail.com>
2019-05-05 14:48:33 -0300

    Implementing commands of app module
    Signed-off-by: Matheus T. dos Santos <tenoriomatheus0@gmail.com>

--- client/application/application_data.py
+++ client/application/application_data.py
@@ -7,12 +7,21 @@ from client.data_paths import base_dir, app_dir
 @dataclass
 class ApplicationData(Serializable):
     name: str
-    key: bytes
+    key: bytes        # 16 bytes
+    key_index: bytes  # 12 bits
+    network: str
     nodes: List[str]
 
-    def __init__(self, name, key, nodes, filename=''):
+    def __init__(self, name, key, key_index, network, nodes=[]):
         super().__init__(filename=base_dir + app_dir + name + '.yml')
 
         self.name = name
         self.key = key
+        self.key_index = key_index
+        self.network = network
         self.nodes = nodes
+
+    def __repr__(self):
+        return f'Name: {self.name}\nKey: {self.key}\n' \
+               f'Key Index: {self.key_index}\nNetwork: {self.network}\n' \
+               f'Nodes: {self.nodes}'
