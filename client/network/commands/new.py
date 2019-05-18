646fcc399fb9c7bb812a8052713ffdf2ff8c4a6e
Matheus T. dos Santos <tenoriomatheus0@gmail.com>
2019-05-17 21:31:10 -0300

    Add parse template to new CLI

--- client/network/commands/new.py
+++ client/network/commands/new.py
@@ -1,6 +1,8 @@
 from Crypto.Random import get_random_bytes
 from client.network.network_data import NetworkData, net_name_list, \
                                         net_key_list, net_key_index_list
+from common.file import file_helper
+from common.template import template_helper
 import click
 
 
@@ -48,6 +50,46 @@ def random_key_index():
     return None
 
 
+def parse_template(ctx, param, value):
+    if not value:
+        return value
+
+    template = file_helper.read(value)
+    if not template:
+        raise click.BadParameter(f'File "{value}" not found')
+
+    try:
+        name = template_helper.get_field(template, 'name')
+    except Exception:
+        raise click.BadParameter(f'Field "name" not found in template file '
+                                 f'"{value}"')
+
+    try:
+        key = template_helper.get_field(template, 'key')
+    except Exception:
+        key = random_key()
+
+    key_index = random_key_index()
+    validate_key_index(key_index)
+
+    if len(key) < 32:
+        key = bytes.fromhex(key) + bytes((32 - len(key)) // 2)
+    elif len(key) > 32:
+        key = bytes.fromhex(key)[0:32]
+    else:
+        key = bytes.fromhex(key)
+
+    net_data = NetworkData(name=name, key=key,
+                           key_index=bytes.fromhex(key_index),
+                           iv_index=bytes(4))
+    net_data.save()
+
+    click.echo(click.style('A new network was created', fg='green'))
+    click.echo(click.style(str(net_data), fg='green'))
+
+    ctx.exit()
+
+
 @click.command()
 @click.option('--name', '-n', type=str, default='',
               help='Specify the name of network', callback=validate_name)
@@ -56,14 +98,16 @@ def random_key_index():
 @click.option('--template', '-t', type=str, default='',
               help='Specify a YAML template file. A file example is shown in'
                    ' net_template.yml. This template file must contain the '
-                   '"name" keyword.')
+                   '"name" keyword.', callback=parse_template, is_eager=True)
 def new(name, key, template):
     '''Create a new network'''
     key_index = random_key_index()
     validate_key_index(key_index)
 
     if len(key) < 32:
-        key = bytes((32 - len(key)) // 2) + bytes.fromhex(key)
+        key = bytes.fromhex(key) + bytes((32 - len(key)) // 2)
+    elif len(key) > 32:
+        key = bytes.fromhex(key)[0:32]
     else:
         key = bytes.fromhex(key)
 
