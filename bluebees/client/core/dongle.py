f4289829712240f5310acbde5d7b9efd83f74049
Matheus Tenorio dos Santos <tenoriomatheus0@gmail.com>
2019-07-05 14:08:55 -0300

    Add a option to search the dongle serial port automatically
    Signed-off-by: Matheus Tenorio dos Santos <tenoriomatheus0@gmail.com>

--- bluebees/client/core/dongle.py
+++ bluebees/client/core/dongle.py
@@ -4,6 +4,9 @@ from asyncserial import Serial
 from dataclasses import dataclass
 from bluebees.common.client import Client
 from bluebees.common.logging import log_sys, INFO, DEBUG
+from serial.tools.list_ports import comports
+from asyncio import wait_for
+from serial import SerialException
 
 
 @dataclass
@@ -19,6 +22,101 @@ class SerialMessage:
     intms: bytes
     content_b64: bytes
     address: bytes
+
+
+class SearchDongle:
+
+    def __init__(self, loop, baudrate=115200):
+        self.log = log_sys.get_logger('dongle.serial')
+        self.log.set_level(INFO)
+
+        self.loop = loop
+        self.baudrate = baudrate
+        self.expected_result = b'@reset 0 00 bm9uZQ==\r\n***** BLE Mesh Dongle v1.0 *****\r\n'
+
+    async def _wait_response(self, ser, port: str) -> bool:
+        line = b''
+        while True:
+            data = await ser.read()
+            line += data
+            if b'\r\n' in line:
+                self.log.debug(f'Result: {line}')
+                if self.expected_result in line:
+                    self.log.success(f'Dongle found at {port}')
+                    return True
+
+    def _split_data(self, data: bytes):
+        datas = []
+        part = b''
+
+        for byte in data:
+            if len(part) >= 5:
+                datas.append(part)
+                part = b''
+            part += byte.to_bytes(1, 'big')
+
+        if len(part) > 0:
+            datas.append(part)
+
+        return datas
+
+    async def _write_on_serial(self, ser, msg: SerialMessage):
+        data = b'@'
+        data += msg.msg_type
+        data += b' '
+        data += msg.xmit
+        data += b' '
+        data += msg.intms
+        data += b' '
+        data += msg.content_b64
+        data += b'\r\n'
+
+        datas = self._split_data(data)
+        for d in datas:
+            await ser.write(d)
+            await asyncio.sleep(0.005)
+
+        await asyncio.sleep(1)
+
+    async def _try_connect(self, ser, port: str, timeout: int) -> bool:
+        # send reset cmd
+        serial_msg = SerialMessage(msg_type=b'reset', xmit=b'0',
+                                   intms=b'00',
+                                   content_b64=b64.b64encode(b'none'),
+                                   address=None)
+        try:
+            await wait_for(self._write_on_serial(ser, serial_msg),
+                           timeout=timeout)
+        except asyncio.TimeoutError:
+            self.log.debug(f'Dongle not found at {port}')
+            return False
+
+        # wait response
+        try:
+            return await wait_for(self._wait_response(ser, port),
+                                  timeout=timeout)
+        except asyncio.TimeoutError:
+            self.log.debug(f'Dongle not found at {port}')
+            return False
+
+    def search(self) -> [str]:
+        valid_ports = []
+        ports = comports()
+        for port, _, _ in sorted(ports):
+            try:
+                self.log.info(f'Testing at {port} serial port...')
+
+                ser = Serial(self.loop, port, self.baudrate)
+
+                result = self.loop.run_until_complete(self._try_connect(ser, port, 2))
+
+                if result:
+                    valid_ports.append(port)
+
+            except SerialException:
+                self.log.warning(f'Serial port {port} not available')
+
+        return valid_ports
 
 
 class Dongle(Client):
@@ -44,14 +142,14 @@ class Dongle(Client):
             b'prov': []
         }
 
-        self.loop.run_until_complete(self.reset_dongle())
+        self.loop.run_until_complete(self._reset_dongle())
 
         self.all_tasks += [self._read_serial_task(),
                            self._transport_message_task(),
                            self._write_serial_task(),
                            self._clear_caches_task()]
 
-    async def reset_dongle(self):
+    async def _reset_dongle(self):
         line = b''
         expected_result = b'***** BLE Mesh Dongle v1.0 *****\r\n'
         for x in range(3):
